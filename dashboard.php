3cc4f55ddd9cc34ba00067b7c999c2eb55293b6c
Maiker Helmet Maria <maiker.brasil@gmail.com>
2018-10-06 19:10:43 -0400

    Transferência de tipo de arquivos feitas no site!
    Agora o site está com extenções PHP no qual será usado para hospedagem no servidor posteriormente.

---
+++ dashboard.php
@@ -0,0 +1,183 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard - Maiker</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="interface toFade">
+        <div class="menu" id="MenuPane">
+            <img src="src/Logo.png" alt="" class="logo" id="logohome">
+            <ul class="lista">
+                <a class="link" id="dash">
+                    <li>
+                        <i class="fas fa-th"></i>
+                    </li>
+                </a>
+                <a class="link ativo" id="proj">
+                    <li>
+                        <i class="fas fa-clipboard-list"></i>
+                    </li>
+                </a>
+                <a class="link" id="tare">
+                    <li>
+                        <i class="fas fa-tasks"></i>
+                    </li>
+                </a>
+                <a class="link" id="config">
+                    <li>
+                        <i class="fas fa-cogs"></i>
+                    </li>
+                </a>
+            </ul>
+        </div>
+        <div class="topo">
+            <div class="menuBtn">
+                <i class="fas fa-list" id="menuToggle"></i>
+                <h4 class="titulo">Dashboard</h4>
+            </div>
+            <div id="opt" class="options activ">
+                <div class="btn" id="novo">
+                    <i class="fas fa-plus"></i>
+                </div>
+                <div id="edit" class="btn">
+                    <i class="fas fa-pen"></i>
+                </div>
+                <div id="excluir" class="btn">
+                    <i class="fas fa-trash"></i>
+                </div>
+            </div>
+        </div>
+        <div class="view viewExpand" id="ViewPane">
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COP</label>
+                <label for="" class="nome">Criador e Organizador de Projetos</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check"></i>
+            </div>
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COPSite</label>
+                <label for="" class="nome">Criador e Organizador de Projetos Online</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check final"></i>
+            </div>
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COP</label>
+                <label for="" class="nome">Criador e Organizador de Projetos</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check"></i>
+            </div>
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COP</label>
+                <label for="" class="nome">Criador e Organizador de Projetos</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check final"></i>
+            </div>
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COP</label>
+                <label for="" class="nome">Criador e Organizador de Projetos</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check"></i>
+            </div>
+            <div class="card">
+                <img src="src/Logo.png" alt="" class="logopro">
+                <label for="" class="titulo">COP</label>
+                <label for="" class="nome">Criador e Organizador de Projetos</label>
+                <div class="time">
+                    <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                    <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                    <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                    <img src="src/maiker.jpg" class="foto " id="maiker">
+                </div>
+                <label for="" class="data">06/07/2010</label>
+                <i class="fas fa-check"></i>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#logohome').click(function() {
+                window.location.href = "index.html";
+            });
+            $('div.card').click(function() {
+                window.location.href = "index.html";
+            });
+            $('i#menuToggle').click(function() {
+                $('#MenuPane').toggleClass('menuactiv');
+                $('#ViewPane').toggleClass('viewExpand');
+            });
+            $('a.link').click(function(event) {
+                var id = $(this).attr('id');
+                $('a.link').removeClass('ativo');
+                $('div#opt').removeClass('activ');
+                if (id === 'dash') {
+                    $('#dash').addClass('ativo');
+                    $('h4.titulo').text("Dashboard");
+                    $('title').text("Dashboard - Maiker");
+                } else if (id === 'proj') {
+                    $('#proj').addClass('ativo');
+                    $('div#opt').addClass('activ');
+                    $('h4.titulo').text("Projetos");
+                    $('title').text("Projetos - Maiker");
+                } else if (id === 'tare') {
+                    $('#tare').addClass('ativo');
+                    $('div#opt').addClass('activ');
+                    $('h4.titulo').text("Tarefas");
+                    $('title').text("Tarefas - Maiker");
+                } else if (id === 'config') {
+                    $('a.link#config').addClass('ativo');
+                    $('h4.titulo').text("Configurações");
+                    $('title').text("Configurações - Maiker");
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>C.O.P</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="interface toFade">
+        <div class="navHome">
+            <div class="logonav">
+                <img src="src/Logo.png" alt="" class="logonav">
+                <label for="">COP</label>
+            </div>
+            <ul class="lista">
+                <a href="login.html" class="link">
+                    <li>
+                        <i class="fas fa-sign-in-alt"></i>
+                        <label for="">Logar</label>
+                    </li>
+                </a>
+            </ul>
+        </div>
+        <div class="viewHome">
+            <h3 class="titulo">COP</h3>
+            <h4 class="subtitulo">Coordenador e organizador de projetos.</h4>
+            <img src="src/Dash.PNG" alt="" class="preview">
+            <p class="desc">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
+                desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <label for="">Equipe de desenvolvimento</label>
+            <div class="sobre">
+                <img src="src/kassio.jpg" alt="" class="foto" id="kassio">
+                <img src="src/mayque.jpg" alt="" class="foto" id="mayque">
+                <img src="src/hagatta.jpg" alt="" class="foto" id="hagatta">
+                <img src="src/maiker.jpg" class="foto " id="maiker">
+            </div>
+            <label for="" class="nomeDev" id="dev">Desenvolvedores</label>
+            <p class="descDev" id="infodev">Desenvolvedores que fizeram o site baseado no programa Desktop COP!</p>
+            <br><br><br><br><br><br><br><br><br>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js "></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js "></script>
+    <script src="js/main.js "></script>
+
+</body>
+
+</html>
