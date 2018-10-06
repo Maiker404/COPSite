6c498ef51959c60356bc2dfc2a6a2b181dab2fb0
Maiker Helmet Maria <maiker.brasil@gmail.com>
2018-10-06 19:30:59 -0400

    Apenas icone do site no titulo do Browser!

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="src/Logo.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>C.O.P</title>
